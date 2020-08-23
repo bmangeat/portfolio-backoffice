d407f2b1793a852931a4c1ed7c490fb9597b4079
bmangeat <brice.mangeat@gmail.com>
2020-08-23 18:39:51 +0200

    fix: test remove https

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = 'https://portfolio-api-bricemangeat.herokuapp.com/api/test/'
+const API_URL = 'http://portfolio-api-bricemangeat.herokuapp.com/api/test/'
 
 class UserService {
     getPublicContent() {
