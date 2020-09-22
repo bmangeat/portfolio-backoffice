2f265143f0ab5cf30febb0193dded63f2099ed2d
bmangeat <brice.mangeat@gmail.com>
2020-09-22 12:58:51 +0200

    :art: Adapt profile component

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,46 +1,40 @@
-import React, {Component} from "react"
+import React, { useState } from "react"
+
+// Import services
 import AuthService from "../services/auth.service"
 
-export default class Profile extends Component{
-    constructor(props) {
-        super(props);
+const Profile = () => {
+    const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser())
 
-        this.state = {
-            currentUser: AuthService.getCurrentUser()
-        }
-    }
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>
+                   {currentUser.username} Profile
+                </h3>
+            </header>
+            <p>
+                <strong>Token : </strong>
+                {currentUser.accessToken.substring(0,20)} ... {" "}
+                {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+            </p>
 
-    render() {
-        const {currentUser} = this.state
+            <p>
+                <strong>ID : </strong>
+                {currentUser.id}
+            </p>
 
-        return (
-            <div className="container">
-                <header className="jumbotron">
-                    <h3>
-                        <strong>{currentUser.username}</strong> Profile
-                    </h3>
-                </header>
-                <p>
-                    <strong>Token : </strong>
-                    {currentUser.accessToken.substring(0, 20)} ... {" "}
-                    {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-                </p>
-                <p>
-                    <strong>ID : </strong>
-                    {currentUser.id}
-                </p>
-                <p>
-                    <strong>Email : </strong>
-                    {currentUser.email}
-                </p>
-                <strong>Authorities</strong>
-                <ul>
-                    {currentUser.roles &&
-                    currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-                </ul>
-
-            </div>
-        )
-    }
-
+            <p>
+                <strong>Email : </strong>
+                {currentUser.email}
+            </p>
+            <strong>Authorities</strong>
+            <ul>
+                {currentUser.roles &&
+                currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+            </ul>
+        </div>
+    )
 }
+
+export default Profile
