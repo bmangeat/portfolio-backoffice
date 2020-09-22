abb3fc755ffd0d1fc02fbd5f916cc50e74eab161
bmangeat <32402124+bmangeat@users.noreply.github.com>
2020-09-22 07:52:51 +0000

    Deploying to gh-pages from  @ 4c5caca4e3171ddaf64f81db52291acd6ab92f1e 🚀

--- precache-manifest.f27c7e6004efc96e8c58e05fa1aa1c68.js
+++ precache-manifest.f27c7e6004efc96e8c58e05fa1aa1c68.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6e388050fc09c458729824e57458feb2",
+    "revision": "a8e3d4d18c69e23aa850e081f1412374",
     "url": "/portfolio-backoffice/index.html"
   },
   {
-    "revision": "0a257d0883beb0dad411",
+    "revision": "a76fb41ea4cc9bd5cb20",
     "url": "/portfolio-backoffice/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "d46471dafa4e6407d9bc",
+    "revision": "a838de1ba8805e21619d",
     "url": "/portfolio-backoffice/static/css/main.2505b687.chunk.css"
   },
   {
-    "revision": "0a257d0883beb0dad411",
-    "url": "/portfolio-backoffice/static/js/2.db6c452d.chunk.js"
+    "revision": "a76fb41ea4cc9bd5cb20",
+    "url": "/portfolio-backoffice/static/js/2.5ba37d89.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio-backoffice/static/js/2.db6c452d.chunk.js.LICENSE.txt"
+    "url": "/portfolio-backoffice/static/js/2.5ba37d89.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d46471dafa4e6407d9bc",
-    "url": "/portfolio-backoffice/static/js/main.318350c2.chunk.js"
+    "revision": "a838de1ba8805e21619d",
+    "url": "/portfolio-backoffice/static/js/main.153e2954.chunk.js"
   },
   {
     "revision": "abe72be1ebcc080a02ba",
